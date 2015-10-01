58676d595c09dcced11dcc16e88d9b2d83efa1eb
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-10-01 15:48:40 +0300

    finally comlete crud for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,51 @@
 class PostsController < ApplicationController
 
-  def new
+  def index
+    @posts = Post.all
   end
+  def new
+    @post = Post.new
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
 
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
     @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html #show.html.erb
+      format.json { render json: @post }
+      format.xml  { render xml: @post }
+    end
   end
 
-  def index
-    @posts = Post.all
-  end
+  def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
   private
   def post_params
     params.require(:post).permit(:title, :body)
