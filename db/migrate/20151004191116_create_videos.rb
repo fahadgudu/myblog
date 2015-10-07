703c6cdaa671e0f9171362bc904c9b36cbd13863
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-10-07 17:24:33 +0300

    remove redundant model user

--- db/migrate/20151004191116_create_videos.rb
+++ db/migrate/20151004191116_create_videos.rb
@@ -1,7 +1,6 @@
 class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
-      t.references :user, index: true, foreign_key: true
       t.string :title
       t.string :body
 
