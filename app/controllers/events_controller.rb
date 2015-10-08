6afb67f6cc07a609055a49a2a55f0356d54780ad
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-10-08 18:45:52 +0300

    add events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,58 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+    @event.build_image_attach
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = ReportPdf.new(@event)
+        send_data pdf.render, filename: 'report.pdf', type: 'application/pdf'
+      end
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :body, image_attributes: [:data])
+  end
+end
